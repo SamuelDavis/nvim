3f637ee06a89a78e36889a220953c5b83fc4191b
Samuel Davis <sdavis@domaintools.com>
2023-05-01 17:42:45 -0400

    group remaps

--- init.lua
+++ init.lua
@@ -123,34 +123,6 @@ cmp.setup({
     },
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set("n", "<leader>D", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set("n", "<leader>wl", function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>f", function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
-    end,
-})
-
 vim.g.prettier = {
     ["autoformat"] = 1,
     ["autoformat_require_pragma"] = 0,
@@ -181,3 +153,27 @@ vim.keymap.set("n", "<leader>fd", Telescope.lsp_definitions, {})
 -- find references
 vim.keymap.set("n", "<leader>fr", Telescope.lsp_references, {})
 
+-- lsp
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set("n", "<leader>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
