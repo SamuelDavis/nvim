9ef661cbbe9953c78d4d1667c0551be1e54be926
Samuel Davis <samueljakdavis@gmail.com>
2025-01-20 09:44:40 -0500

    init gleam lsp

--- init.lua
+++ init.lua
@@ -280,6 +280,7 @@ require("lazy").setup({
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+			lspconfig.gleam.setup({})
 			lspconfig.gdscript.setup({
 				capabilities = capabilities,
 				settings = {},
