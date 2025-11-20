35dd8e9ab5d68f4ee95d544d3f9d89a03c3e1315
Samuel Davis <samueljakdavis@gmail.com>
2025-11-20 08:30:29 -0500

    unused var

--- init.lua
+++ init.lua
@@ -76,7 +76,6 @@ local function map(key, fn, desc, mode)
 	vim.keymap.set(mode or "n", "<leader>" .. key, fn, { desc = desc })
 end
 
-local prefixes = {}
 local function keymap_prefix(prefix_key, prefix_desc)
 	return function(key, fn, desc, mode)
 		local prefix = prefix_key
