c4e68eef8c1ae447c5b53c25a3731c9b65ed666e
Samuel Davis <samueljakdavis@gmail.com>
2026-04-12 13:58:38 -0400

    update deps

--- init.lua
+++ init.lua
@@ -482,7 +482,6 @@ require("lazy").setup({
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs",
 		opts = {
 			auto_install = true,
 			highlight = { enable = true },
